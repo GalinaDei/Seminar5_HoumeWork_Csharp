6a87e53d57b53be317612ad8ed09d569702b9df2
Galina <gal-dej@yandex.ru>
2022-10-17 14:33:40 +0300

    Task has complited

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -3,4 +3,39 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+int[] CreateArray (int num)
+{
+    int[] array = new int [num];
+    Random rnd = new Random(num);
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=rnd.Next(100,999);
+    }
+    return (array);
+}
+void PrintArray (int[]array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i!=array.Length-1)
+        {Console.Write($"{array[i]}, ");}
+       else {Console.Write($"{array[i]}] -> ");}
+    }
+}
+void ElementSummCount (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2!=0)
+        {count= count + array[i];}
+        else{}
+        
+    }    
+    Console.WriteLine($"{count}");
+}
 
+int[]resultArray= CreateArray (3);
+PrintArray(resultArray);
+ElementSummCount(resultArray);
